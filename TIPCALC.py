dd2fb38c65c33bde580db4213f85473f1aef4c0d
Chandan_Malakar <chandanmalakar6209@gmail.com>
2023-12-05 10:54:12 +0530

    CHANGES IN THE TIP CALC

--- TIPCALC.py
+++ TIPCALC.py
@@ -1,6 +1,8 @@
 print("Welcome to the Tip Calculator")
 total=float(input("What is the total bill amount ? $"))
-num=int(input("How many people to split the bill? "))
-tip=int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-amt=float((total/num)*(12/100))
-print("Each person should pay : $ {amt}")
+num=float(input("How many people to split the bill? "))
+tip=float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+
+amt=float(total+(total*(tip/100)))
+new_amt=(amt/num)
+print("Each person should pay : $"+str(new_amt))
