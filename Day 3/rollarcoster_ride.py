191161a2662b04bfca0b91092bbe64981dcf4e86
Chandan_Malakar <chandanmalakar6209@gmail.com>
2023-12-11 11:15:29 +0530

    Day 3

---
+++ Day 3/rollarcoster_ride.py
@@ -0,0 +1,9 @@
+# THIS IS A ROLLER COASTER RIDE ELIGIBILITY CHECKING PROGRAM
+
+
+height=float(input("Enter your height in cm: "))
+if height>=120:
+    print("You can ride the roller coaster")
+else:
+    print("You cannot ride the roller coaster")
+
