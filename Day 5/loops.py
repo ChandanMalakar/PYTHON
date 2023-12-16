83e3b1205a5744b0a82c8bb05a6d252dd0f132de
Chandan_Malakar <chandanmalakar6209@gmail.com>
2023-12-16 18:15:02 +0530

    Day 5 Completed

---
+++ Day 5/loops.py
@@ -0,0 +1,11 @@
+fruits=["Apple", "Peach", "Pear"]
+length=len(fruits)
+print(length)
+#OR 
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "Pie")
+#OR
+#for loops in python
+for i in range(0,length):
+    print(fruits[i])
