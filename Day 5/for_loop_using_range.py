0a4f1f1aa006c20ded2e8651c67ef0d033febf3a
Chandan_Malakar <chandanmalakar6209@gmail.com>
2023-12-17 15:14:00 +0530

    For Loops Using Range Function

---
+++ Day 5/for_loop_using_range.py
@@ -0,0 +1,14 @@
+# For loop using range function
+
+# syntax :
+
+# for [variable] in range (start,end,step):
+#   [body of the loop]
+
+for number in range(1,11,2):
+    print(number)
+
+sum=0
+for number in range(1,101):
+    sum+=number
+print(sum)
