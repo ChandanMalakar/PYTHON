306f50b4295c4dd573f094305028618dd25e65f8
Chandan_Malakar <chandanmalakar6209@gmail.com>
2023-12-22 11:26:30 +0530

    Almost Completed

--- Day 8/caeser_cipher.py
+++ Day 8/caeser_cipher.py
@@ -26,22 +26,29 @@ print(''' ██████╗ █████╗ ███████╗█�
 list=['encode', 'decode']
 user_choice=input("Type 'encode' to encrypt, type 'decode' to decrypt :\n")
 new_message=""
-if user_choice in list:
-    message=input("Type your message :\n")
-    shift_number=int(input("Type the shift number :\n"))
-    if user_choice=='encode':
-        for i in message:
-            dup=ord(i)
-            dup+=shift_number
-            dup=chr(dup)
-            new_message+=dup
-        print(f"Your encoded message is : {new_message}")
+once_again=False
+while not once_again:
+    if user_choice in list:
+        message=input("Type your message :\n")
+        shift_number=int(input("Type the shift number :\n"))
+        if user_choice=='encode':
+            for i in message:
+                dup=ord(i)
+                dup+=shift_number
+                dup=chr(dup)
+                new_message+=dup
+            print(f"Your encoded message is : {new_message}")
+        else:
+            for i in message:
+                dup=ord(i)
+                dup-=shift_number
+                dup=chr(dup)
+                new_message+=dup
+            print(f"Your decoded message is : {new_message}")
     else:
-        for i in message:
-            dup=ord(i)
-            dup-=shift_number
-            dup=chr(dup)
-            new_message+=dup
-        print(f"Your decoded message is : {new_message}")
-else:
-    print("Invalid choice")
+        print("Invalid choice")
+    take=input("Type 'Yes' if you want to go again OR Type 'No' if you don't :\n")
+    if(take=='Yes'):
+        once_again=False
+    else:
+        once_again=True
