9579ac24d31918374b71d772caf1b23a1de63ac9
Chandan_Malakar <chandanmalakar6209@gmail.com>
2024-02-12 22:41:43 +0530

    Day 12 Completed

---
+++ Day 12/NumberGuessingGame.py
@@ -0,0 +1,41 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+level=input("Choose a difficulty. Type 'easy' or 'hard' : ")
+
+
+guess=random.randint(1,100)
+
+
+if level=='easy':
+    no_of_attempts=5
+    print(f"You have {no_of_attempts} attempts remaining to guess the number ")
+else:
+    no_of_attempts=10
+    print(f"You have {no_of_attempts} attempts remaining to guess the number ")
+
+run_until=False
+
+while not run_until:
+    my_guess=int(input("Make a guess: "))
+
+    if my_guess>guess:
+        print("Too high.")
+        print("Guess again.")
+        no_of_attempts-=1
+        print(f"You have {no_of_attempts} attempts remaining to guess the number.")
+    elif my_guess<guess:
+        print("Too Low.")
+        print("Guess again.")
+        no_of_attempts-=1
+        print(f"You have {no_of_attempts} attempts remaining to guess the number.")
+    elif my_guess==guess:
+        print("Correct Guess! You win!")
+        run_until=True
+    
+    if no_of_attempts==0:
+        print("You've run out of guesses, you lose.")
+        run_until=True
+    
+print("The number was ",guess)
--- Day 12/scope.py
+++ Day 12/scope.py
@@ -7,13 +7,11 @@
 # print(f"outside function {outsider}")
 # access()
 
-
 # Local Scope
 # any variable value which is inside 
 # a function
 # can't be accessed from outside
 # this is known as local scope
-
 
 def potion_drinking():
     potion_maker=12
@@ -21,7 +19,6 @@ def potion_drinking():
 
 potion_drinking()
 # print(potion_maker) # error
-
 
 # Global Scope
 
@@ -31,18 +28,64 @@ potion_drinking()
 
 health_max=10
 
-def potion_drinking():
-    print(health_max)
+def game():
+    def potion_drinking():
+        print(health_max)
 
-potion_drinking()
-
+    potion_drinking()
 
 # local variables are those which are defined inside the function
 # global variables are those which are defined outside the function
 
-
 # this concept applies not only to 
 # variables but also to functions and also this concept applies
 # to anything you name
+
+# anything you give a name to , it may be functions,variables,etc has a
+# namespace.
+
+# there is no block scope means there is no use of curly braces to express
+# a certain block of code , instead indentation works in python.
+    
+# if you create a variable within a function, then its only available within
+# that function only.
+
+# Modifying Global Scope
+    
+# all the changes made in global variable won't affect the local one
+# it is the same for the local variable.
+
+# there is no block scope in python
+    
+# only local scope is there in functions and classes
+    
+# not in if statements
+
+game_level=3
+# def create_enemy():
+enemies=["Skeleton","Zombie","Alien"]
+if game_level<5:
+    new_enemy=enemies[0]
+    
+print(new_enemy)
+
+enemies=1
+
+def increase_enemies():
+    enemies+=1
+    print(f"enemies inside function {enemies}")
+
+increase_enemies()
+print(f"enemies outside function {enemies}")
+
+
+# Global Constants
+
+# constants are those variables whose value never changes
+# they are written in capital letters
+# and are separated by underscores
+
+PI=3.14159
+URL="https://www.google.com"
 
 
