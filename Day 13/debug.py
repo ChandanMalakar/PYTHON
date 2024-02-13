943a016dcb4b79261a8ef043abc1946968624ad6
Chandan_Malakar <chandanmalakar6209@gmail.com>
2024-02-13 21:09:32 +0530

    Day 13 almost finished

---
+++ Day 13/debug.py
@@ -0,0 +1,48 @@
+# # Describe the problem
+# def my_function():
+#     # for i in range(1,20)://debugged part
+#     for i in range(1,21):
+#         if i==20:
+#             print("you got it")
+# my_function()
+
+
+# # Reproduce the bug
+# from random import randint
+# dice_imgs=["1","2","3","4","5","6"]
+# dice_num=randint(0,5)
+# print(dice_imgs[dice_num])
+
+
+# # Play Computer
+# year=int(input("What's your year of birth ? \n"))
+# if year>1980 and year<1994:
+#     print("You're a millenial.")
+# elif year>=1994:
+#     print("You're a Gen Z.")
+
+
+# # Fiz the Errors
+# age=int(input("How old are you ? "))
+# if age>18:
+#     print(f"You can drive at age {age}")
+
+
+# # Print is Your Friend
+# pages=0
+# words_per_page=0
+# pages=int(input("Number of Pages : "))
+# words_per_page=int(input("Number of words per page : "))
+# total_words=pages*words_per_page
+# print(total_words)
+
+
+# # Use a Debugger
+# def mutate(a_list):
+#     b_list=[]
+#     for item in a_list:
+#         new_item=item*2
+#         b_list.append(new_item)
+#     print(b_list)
+
+# mutate([1,2,3,4,5,8,13])
